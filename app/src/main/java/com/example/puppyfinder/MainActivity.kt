19b32c74bbf470a4f3ff673354e6de92ef1233bd
ramanpreetkaur <preetraman905@gmail.com>
2023-10-26 15:19:31 -0400

    adding search bar

--- app/src/main/java/com/example/puppyfinder/MainActivity.kt
+++ app/src/main/java/com/example/puppyfinder/MainActivity.kt
@@ -1,46 +1,66 @@
-package com.example.puppyfinder
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.puppyfinder.ui.theme.PuppyFinderTheme
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PuppyFinderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            val searchQuery = remember { mutableStateOf("") }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                SearchBar(searchQuery.value, onQueryChange = { searchQuery.value = it }, {})
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SearchBar(
+        query: String, onQueryChange: (String) -> Unit, onSearchButtonClick: () -> Unit
+    ) {
+        TextField(
+            value = query,
+            onValueChange = { newQuery -> onQueryChange(newQuery) },
+            label = { Text("find your pup") },
+            singleLine = true,
+            modifier = Modifier
+                .background(Color.LightGray, shape = RoundedCornerShape(5.dp))
+                .fillMaxWidth()
+                .shadow(1.dp),
+            textStyle = TextStyle(Color.Blue, fontFamily = FontFamily.Monospace, fontSize = 14.sp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = { onSearchButtonClick() }
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PuppyFinderTheme {
-        Greeting("Android")
+        )
     }
 }
