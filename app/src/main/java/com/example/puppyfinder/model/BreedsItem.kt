8e5f94471773b0f5a4491ec2b83beaff36abac0b
ramanpreetkaur <preetraman905@gmail.com>
2023-10-26 17:23:04 -0400

    adding serialization dependency

--- app/src/main/java/com/example/puppyfinder/model/BreedsItem.kt
+++ app/src/main/java/com/example/puppyfinder/model/BreedsItem.kt
@@ -1,17 +1,32 @@
 package com.example.puppyfinder.model
 
+import com.google.gson.annotations.SerializedName
+
 data class BreedsItem(
-    val bred_for: String,
-    val breed_group: String,
-    val country_code: String,
+    @SerializedName("bred_for")
+    val bredFor: String,
+    @SerializedName("breed_group")
+    val breedGroup: String,
+    @SerializedName("country_code")
+    val countryCode: String,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("height")
     val height: Height,
+    @SerializedName("history")
     val history: String,
+    @SerializedName("id")
     val id: Int,
-    val life_span: String,
+    @SerializedName("life_span")
+    val lifeSpan: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("origin")
     val origin: String,
-    val reference_image_id: String,
+    @SerializedName("reference_image_id")
+    val referenceImageId: String,
+    @SerializedName("temperament")
     val temperament: String,
+    @SerializedName("weight")
     val weight: Weight
 )
