902a87e0131d3a99ac017b1f7ecdb7b33c7ed54a
ramanpreetkaur <preetraman905@gmail.com>
2024-03-19 19:20:44 -0400

    upload image api returning 500

--- app/src/main/java/com/example/puppyfinder/PupApi.kt
+++ app/src/main/java/com/example/puppyfinder/PupApi.kt
@@ -4,7 +4,13 @@ import com.example.puppyfinder.model.Breed
 import com.example.puppyfinder.model.BreedImage
 import com.example.puppyfinder.model.BreedInfo
 import com.example.puppyfinder.model.ImagesItem
+import com.example.puppyfinder.model.PostImage
+import okhttp3.MultipartBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -21,6 +27,11 @@ interface PupApi {
     @GET("v1/images/{id}")
     suspend fun getImage(@Path("id") id: String) : BreedImage
 
-    @GET("v1/images/search?limit=20")
+    @Headers("x-api-key:live_LYYFOYLv9e9YKnnIrhm186pgGNpFo9SQhEVyDfR1BKClyjWPggToWerirHjwZm0a")
+    @GET("v1/images/search?limit=10")
     suspend fun getImages() : List<ImagesItem>
+
+    @Headers("Content-Type:multipart/form-data", "x-api-key:live_LYYFOYLv9e9YKnnIrhm186pgGNpFo9SQhEVyDfR1BKClyjWPggToWerirHjwZm0a")
+    @POST("/v1/images/upload")
+    suspend fun uploadImage(@Body requestBody: MultipartBody): PostImage
 }
--- app/src/main/java/com/example/puppyfinder/app/ui/BreedScreen.kt
+++ app/src/main/java/com/example/puppyfinder/app/ui/BreedScreen.kt
@@ -1,9 +1,15 @@
 package com.example.puppyfinder.app.ui
 
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,7 +20,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -22,6 +32,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.puppyfinder.R
 import com.example.puppyfinder.viewmodel.PupViewModel
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.ByteArrayOutputStream
+import java.io.File
+
 
 /**
  * enum values that represent the screens in the app
@@ -38,13 +56,43 @@ fun BreedFinderAppBar(
     currentScreen: BreedScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    showMenu: Boolean,
+    viewModel: PupViewModel
 ) {
     TopAppBar(
         title = { Text(stringResource(currentScreen.title)) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         ),
+        actions = {
+            if (showMenu) {
+                val context = LocalContext.current
+                val getContent : ActivityResultLauncher<String>
+                var imageUri by remember { mutableStateOf<Uri?>(null) }
+                getContent = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                    imageUri = uri
+                }
+                IconButton(onClick = {
+                    getContent.launch("image/*")
+                    imageUri?.let { it ->
+                        val inputStream = context.contentResolver.openInputStream(it)
+                        val bitmap = inputStream?.use { stream ->
+                            BitmapFactory.decodeStream(stream)
+                        }
+                        bitmap?.let {b ->
+                            // Convert the image to bytes
+                            val byteStream = ByteArrayOutputStream()
+                            b.compress(android.graphics.Bitmap.CompressFormat.JPEG, 100, byteStream)
+                            val byteArray = byteStream.toByteArray()
+                            uploadImage(byteArray, viewModel = viewModel)
+                        }
+                    }
+                }) {
+                    Icon(Icons.Default.AddCircle, contentDescription = "shareMenu")
+                }
+            }
+        },
         modifier = modifier,
         navigationIcon = {
             if (canNavigateBack) {
@@ -57,6 +105,16 @@ fun BreedFinderAppBar(
             }
         }
     )
+}
+
+fun uploadImage(imageUri: ByteArray, viewModel: PupViewModel) {
+    val requestBody = MultipartBody.Builder()
+        .setType(MultipartBody.FORM)
+        .addFormDataPart("file", "great.jpg", RequestBody.create("image/jpeg".toMediaTypeOrNull(), imageUri))
+        .addFormDataPart("sub_id", "")
+        .build()
+
+    viewModel.uploadYourDogImage(body = requestBody)
 }
 
 @Composable
@@ -76,7 +134,9 @@ fun BreedFinderApp(
             BreedFinderAppBar(
                 currentScreen = currentScreen,
                 canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
+                navigateUp = { navController.navigateUp() },
+                showMenu = navController.currentDestination?.route == BreedScreen.Images.name,
+                viewModel = viewModel
             )
         }
     ) { innerPadding ->
